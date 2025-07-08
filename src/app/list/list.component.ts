55172d00d0583ecd015541e174434cee1d94bf1c
Liisi Loomets <loomets@tlu.ee>
2025-07-08 12:29:42 +0300

    add function to format gender

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -13,17 +13,23 @@ export class ListComponent implements OnInit {
   articleService = inject(ArticlesService);
   articleItems = signal<Article[]>([]);
 
-    ngOnInit(): void {
-      this.articleService
-        .getArticleList()
-        .pipe(
-          catchError((err) => {
-            console.log(err);
-            throw err;
-          })
-        )
-        .subscribe((article: Article[]) => {
-          this.articleItems.set(article)
-        });
-    }
+  ngOnInit(): void {
+    this.articleService
+      .getArticleList()
+      .pipe(
+        catchError((err) => {
+          console.log(err);
+          throw err;
+        })
+      )
+      .subscribe((article: Article[]) => {
+        this.articleItems.set(article)
+      });
+  }
+
+  getSexLabel(sex : string): string {
+    if(sex === 'm') return 'Mees';
+    if(sex === 'f') return 'Naine';
+    return '';
+  }
 }
