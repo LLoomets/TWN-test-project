11beea7c4aa733bd84c0993bba7048c481c97398
Liisi Loomets <loomets@tlu.ee>
2025-07-10 15:48:04 +0300

    create component

---
+++ src/app/game-of-life/game-of-life.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-game-of-life',
+  imports: [],
+  templateUrl: './game-of-life.component.html',
+  styleUrl: './game-of-life.component.scss'
+})
+export class GameOfLifeComponent {
+
+}
