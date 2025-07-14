71db66713d7f3c46b5719e2bacd7a63bd11f4572
Liisi Loomets <loomets@tlu.ee>
2025-07-14 13:04:39 +0300

    create and test canvas

--- src/app/game-of-life/game-of-life.component.ts
+++ src/app/game-of-life/game-of-life.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+
+const BOARD_WIDTH = 1000;
+const BOARD_HEIGHT = 400;
+const RESOLUTION = 10; 
 
 @Component({
   selector: 'app-game-of-life',
@@ -6,6 +10,18 @@ import { Component } from '@angular/core';
   templateUrl: './game-of-life.component.html',
   styleUrl: './game-of-life.component.scss'
 })
-export class GameOfLifeComponent {
+export class GameOfLifeComponent implements AfterViewInit {
+  @ViewChild('gameboard', { static: false }) canvas!: ElementRef<HTMLCanvasElement>;
 
+  private ctx!: CanvasRenderingContext2D;
+
+  ngAfterViewInit(): void {
+      this.canvas.nativeElement.width = BOARD_WIDTH;
+      this.canvas.nativeElement.height = BOARD_HEIGHT;
+
+      this.ctx = this.canvas.nativeElement.getContext('2d')!;
+
+      this.ctx.strokeStyle = 'white';
+      this.ctx.strokeRect(450, 150, 100, 100);
+  }
 }
