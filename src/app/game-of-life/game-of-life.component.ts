eaeb0e5bd8eba3d908fcf1fe8134ee03134778cd
Liisi Loomets <loomets@tlu.ee>
2025-07-14 13:18:36 +0300

    log gameboard array

--- src/app/game-of-life/game-of-life.component.ts
+++ src/app/game-of-life/game-of-life.component.ts
@@ -4,24 +4,40 @@ const BOARD_WIDTH = 1000;
 const BOARD_HEIGHT = 400;
 const RESOLUTION = 10; 
 
+export type Bit = 0 | 1;
+export type BitArray = Bit[];
+
 @Component({
   selector: 'app-game-of-life',
   imports: [],
   templateUrl: './game-of-life.component.html',
-  styleUrl: './game-of-life.component.scss'
+  styleUrl: './game-of-life.component.scss',
 })
 export class GameOfLifeComponent implements AfterViewInit {
-  @ViewChild('gameboard', { static: false }) canvas!: ElementRef<HTMLCanvasElement>;
+  @ViewChild('gameboard', { static: false })
+  canvas!: ElementRef<HTMLCanvasElement>;
 
   private ctx!: CanvasRenderingContext2D;
 
+  gameBoard!: BitArray[];
+
   ngAfterViewInit(): void {
-      this.canvas.nativeElement.width = BOARD_WIDTH;
-      this.canvas.nativeElement.height = BOARD_HEIGHT;
+    this.canvas.nativeElement.width = BOARD_WIDTH;
+    this.canvas.nativeElement.height = BOARD_HEIGHT;
 
-      this.ctx = this.canvas.nativeElement.getContext('2d')!;
+    this.ctx = this.canvas.nativeElement.getContext('2d')!;
 
-      this.ctx.strokeStyle = 'white';
-      this.ctx.strokeRect(450, 150, 100, 100);
+    const numOfRows = BOARD_HEIGHT / RESOLUTION;
+    const numOfCols = BOARD_WIDTH / RESOLUTION;
+
+    const oneOrZero = () => (Math.random() > 0.5 ? 1 : 0);
+
+    const board: BitArray[] = new Array(numOfRows)
+      .fill(0)
+      .map(() => new Array(numOfCols).fill(0).map(oneOrZero));
+
+    this.gameBoard = board;
+
+    console.log(this.gameBoard);
   }
 }
